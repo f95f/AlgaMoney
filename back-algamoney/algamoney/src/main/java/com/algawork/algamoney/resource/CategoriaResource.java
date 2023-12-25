3e8fb25fb50abf7b4acd812e511c0dfcfef38d11
f95f <95felipefernandes@gmail.com>
2023-12-24 21:29:28 -0300

    feat: added endpoint for listing categorias.

--- back-algamoney/algamoney/src/main/java/com/algawork/algamoney/resource/CategoriaResource.java
+++ back-algamoney/algamoney/src/main/java/com/algawork/algamoney/resource/CategoriaResource.java
@@ -1,11 +1,63 @@
 package com.algawork.algamoney.resource;
 
+import com.algawork.algamoney.model.Categoria;
+import com.algawork.algamoney.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
+    @Autowired
+    private CategoriaRepository repository;
+
+    @GetMapping
+    public List<Categoria> listar(){
+
+        return repository.findAll();
+
+    }
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
